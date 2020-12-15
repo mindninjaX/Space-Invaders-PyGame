32b543cb0d0f27d5f738657673665f4b8894140a
Rishabh Singh <rishabhsinghs90s@gmail.com>
2020-12-15 18:47:22 +0530

    Fixed the Screen not loading bug

--- Space-Invaders.py
+++ Space-Invaders.py
@@ -9,5 +9,5 @@ screen = pygame.display.set_mode((800,600))
 running = True
 while running:
     for event in pygame.event.get():
-        if event.type == pygame.QUIT():
+        if event.type == pygame.QUIT:
             running = False
